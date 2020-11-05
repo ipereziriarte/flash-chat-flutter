25386771b729363a681ab5dd3b6e373f28659bcd
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2020-11-05 11:52:41 +0100

    Bind button navigation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'login_screen.dart';
+import 'registration_screen.dart';
+
 class WelcomeScreen extends StatefulWidget {
   static const path = 'welcome_screen';
 
@@ -44,7 +47,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.Path);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -62,7 +65,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.path);
                   },
                   minWidth: 200.0,
                   height: 42.0,
