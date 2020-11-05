35b744489877d1f5e91096b3abcd45ae47107051
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2020-11-05 12:39:18 +0100

    Fix issue with vsync not defined see: https://stackoverflow.com/questions/63207206/animationcontroller-the-named-parameter-vsync-isnt-defined

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,7 +10,18 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
