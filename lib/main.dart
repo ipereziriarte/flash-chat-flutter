58940252e8e14206e84e975efdeb53fc836f81d8
Imanol Pérez Iriarte <eltigrequehayenti@gmail.com>
2020-11-08 19:33:24 +0100

    Listen for message collection updates
    Stream works as an observable and can be managed by creating
    a streambuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,11 @@ import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
